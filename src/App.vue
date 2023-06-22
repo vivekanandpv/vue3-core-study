762ec031793998201b5e2a9008eac961beea70fd
vivek <vivekanandpv@outlook.com>
2023-06-22 17:18:44 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <!-- Vite uses script with setup, which is a new proposal. 
   For this demo, I am turning it off till I specifically take it. -->
 <script lang="ts">
+import persons from './data.json';
 export default {
   data() {
     return {
-      isRegistered: true,
+      persons,
     };
   },
 };
@@ -14,9 +15,22 @@ export default {
   <div class="p-5">
     <h2>Vue 3 Learning</h2>
     <hr />
-    <!-- Only expressions are allowed. Statements, such as assignment, 
-    branching, etc are not allowed in templates. -->
-    <p>{{ isRegistered ? 'Registered' : 'Not Registered' }}</p>
+    <div
+      class="alert alert-primary"
+      role="alert"
+      v-for="(person, index) in persons"
+      :key="person.id"
+    >
+      <p>Serial: {{ index }}</p>
+      <p>First Name: {{ person.firstName }}</p>
+      <p>Last Name: {{ person.lastName }}</p>
+      <p>Email: {{ person.email }}</p>
+    </div>
+
+    <!-- index is optional
+    by default index is used as key prop
+    more about key prop: https://vuejs.org/api/built-in-special-attributes.html#key
+     -->
   </div>
 </template>
 
