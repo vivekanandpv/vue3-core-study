9be1ca3268b156954790a01c02ced9419413ffd9
vivek <vivekanandpv@outlook.com>
2023-06-22 17:21:07 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <!-- Vite uses script with setup, which is a new proposal. 
   For this demo, I am turning it off till I specifically take it. -->
 <script lang="ts">
-import persons from './data.json';
 export default {
   data() {
     return {
-      persons,
+      person: {
+        id: 4,
+        firstName: 'Michele',
+        lastName: 'Thorneywork',
+        email: 'mthorneywork3@fastcompany.com',
+        gender: 'Genderqueer',
+        ipAddress: '61.47.243.3',
+      },
     };
   },
 };
@@ -15,22 +21,13 @@ export default {
   <div class="p-5">
     <h2>Vue 3 Learning</h2>
     <hr />
-    <div
-      class="alert alert-primary"
-      role="alert"
-      v-for="(person, index) in persons"
-      :key="person.id"
-    >
-      <p>Serial: {{ index }}</p>
-      <p>First Name: {{ person.firstName }}</p>
-      <p>Last Name: {{ person.lastName }}</p>
-      <p>Email: {{ person.email }}</p>
+    <div class="alert alert-primary" role="alert">
+      <p v-for="(value, key, index) in person">
+        {{ index }} --> {{ key }}: {{ value }}
+      </p>
     </div>
 
-    <!-- index is optional
-    by default index is used as key prop
-    more about key prop: https://vuejs.org/api/built-in-special-attributes.html#key
-     -->
+    <!-- In object iteration, key and index are optional -->
   </div>
 </template>
 
